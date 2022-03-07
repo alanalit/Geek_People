2688b900501cb148b48d268bb2893f82ee45841c
Alana Lima <alanaaa840@gmail.com>
2022-03-07 19:51:08 -0300

    Project created

---
+++ backend/src/main/java/com/devsite/geekPeople/GeekPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.devsite.geekPeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekPeopleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsite/geekPeople/GeekPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsite.geekPeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
